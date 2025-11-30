6643f753bc149caedc33ec476f7f6fe6e52c765e
Aleksey Korotkov <a.korotkov@omp.rugit>
2025-11-30 18:47:58 +0300

    [backend] Fix views

--- backend/core/views.py
+++ backend/core/views.py
@@ -6,6 +6,7 @@ from rest_framework import generics, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .models import Meter, MonthlyCharge, Payment, Property, Reading, Tariff
 from .serializers import (
